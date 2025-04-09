d48d85db8684b2e7651422f42a8480e7a9de3e14
ali mert inal <mert.inal@gmail.com>
2025-04-09 13:38:25 +0200

    navBar component imported

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Routes,Route } from 'react-router'
 import './App.css'
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
+import Navbar from './components/ui/nav-bar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
      <Routes>
       <Route path="/" element={<HomePage/>} />
       <Route path="/product/:productId" element={<ProductPage/>} />

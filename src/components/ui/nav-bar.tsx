0133fa53a820182a279ff84b0fcc8bd3851fcf6a
ali mert inal <mert.inal@gmail.com>
2025-04-09 13:38:11 +0200

    a tag added

--- src/components/ui/nav-bar.tsx
+++ src/components/ui/nav-bar.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>nav-bar</div>
+    <>
+    <nav className='w-full border-0 py-4 lg:px-24 px-10 bg-gray-950'>
+<h1 className='text-3xl text-white'>
+    <a href="/" >ReactNav</a>
+
+</h1>
+    </nav>
+    </>
   )
 }
